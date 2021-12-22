0304ec455d983ff6da18db4baa1446aa21c054e0
elton-costa <tonrc1@hotmail.com>
2021-12-22 16:58:51 -0300

    Add font and colors

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -22,6 +22,7 @@ class ProductItem extends StatelessWidget {
           leading: IconButton(
             onPressed: () {},
             icon: Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
           ),
           title: Text(
             product.title,
@@ -30,6 +31,7 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             onPressed: () {},
             icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
           ),
         ),
       ),
