a9c0560a5bcd5786f457d2547e174f5b21100549
elton-costa <tonrc1@hotmail.com>
2021-12-24 21:38:14 -0300

    Create class product_list

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,16 +1,19 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummy_data.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/widgets/product_item.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
   ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
+    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
