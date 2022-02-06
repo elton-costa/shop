20377ac6bac61b4f7b7ef6834160e6350297e388
elton-costa <tonrc1@hotmail.com>
2022-02-06 10:15:02 -0300

    Send post request

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
+  final _baseUrl = 'https://shop-ec-51e8a-default-rtdb.firebaseio.com';
   List<Product> _items = dummyProducts;
 
   List<Product> get items => [..._items];
@@ -17,6 +20,7 @@ class ProductList with ChangeNotifier {
 
   void saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
+
     final product = Product(
       id: hasId ? data['id'] as String : Random().nextDouble().toString(),
       name: data['name'] as String,
@@ -24,6 +28,7 @@ class ProductList with ChangeNotifier {
       price: data['price'] as double,
       imageUrl: data['imageUrl'] as String,
     );
+
     if (hasId) {
       updateProduct(product);
     } else {
@@ -32,6 +37,18 @@ class ProductList with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    http.post(
+      Uri.parse('$_baseUrl/products.json'),
+      body: jsonEncode(
+        {
+          "name": product.name,
+          "description": product.description,
+          "price": product.price,
+          "imageUrl": product.imageUrl,
+          "isFavorite": product.isFavorite,
+        },
+      ),
+    );
     _items.add(product);
     notifyListeners();
   }
@@ -54,21 +71,22 @@ class ProductList with ChangeNotifier {
     }
   }
 }
-// bool _showFavoriteOnly = false;
 
-//   List<Product> get items {
-//     if (_showFavoriteOnly) {
-//       return _items.where((prod) => prod.isFavorite).toList();
-//     }
-//     return [..._items];
-//   }
+  // bool _showFavoriteOnly = false;
 
-//   void showFavoriteOnly() {
-//     _showFavoriteOnly = true;
-//     notifyListeners();
-//   }
+  // List<Product> get items {
+  //   if (_showFavoriteOnly) {
+  //     return _items.where((prod) => prod.isFavorite).toList();
+  //   }
+  //   return [..._items];
+  // }
 
-//   void showAll() {
-//     _showFavoriteOnly = false;
-//     notifyListeners();
-//   }
+  // void showFavoriteOnly() {
+  //   _showFavoriteOnly = true;
+  //   notifyListeners();
+  // }
+
+  // void showAll() {
+  //   _showFavoriteOnly = false;
+  //   notifyListeners();
+  // }
